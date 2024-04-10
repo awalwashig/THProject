d3f6bcf713ff2853bbe94e225098ed1ebcb31308
awaわ <a970565@163.com>
2024-04-10 22:55:02 +0800

    test

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+﻿const para = document.querySelector("p");
+
+para.addEventListener("click", updateName);
+
+function updateName() {
+    const name = prompt("Enter a new name");
+    para.textContent = `Player 1: ${name}`;
+}
